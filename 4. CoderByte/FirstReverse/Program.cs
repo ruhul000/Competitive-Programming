2d87246058ce1730cfbdee1b67b76d97c94ef02b
Ruhul <ruhul.ewu00@gmail.com>
2024-01-18 20:13:38 +0600

    solved FirstReverse

---
+++ 4. CoderByte/FirstReverse/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class MainClass {
+
+  public static string FirstReverse(string str) {
+
+    char[] stringArray = str.ToCharArray();
+
+    string reverse = String.Empty;
+
+    for (int i = stringArray.Length - 1; i >= 0; i--)
+    {
+        reverse += stringArray[i];
+    }
+    return reverse;
+  }
+
+  static void Main() {  
+
+    // keep this function call here
+    Console.WriteLine(FirstReverse(Console.ReadLine()));
+    
+  } 
+
+}
