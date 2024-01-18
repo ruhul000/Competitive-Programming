db68c18fb099becbf68e394d23fff60438fd80b0
Ruhul <ruhul.ewu00@gmail.com>
2024-01-18 20:02:09 +0600

    solved FirstFactorial

---
+++ 4. CoderByte/FirstFactorial/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class MainClass
+{
+
+    public static int FirstFactorial(int num)
+    {
+        int reuslt = 1;
+        if (num == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            reuslt = num * FirstFactorial(num - 1);
+        }
+        return reuslt;
+    }
+
+    static void Main()
+    {
+
+        // keep this function call here
+        Console.WriteLine(FirstFactorial(Convert.ToInt32(Console.ReadLine())));
+
+    }
+
+}
